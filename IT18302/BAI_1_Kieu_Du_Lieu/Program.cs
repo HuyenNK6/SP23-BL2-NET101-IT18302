1921d1e3ba8461b54ef3ded3fb29768fab70adca
huyennk6 <huyennk6@fpt.edu.vn>
2023-03-06 17:00:31 +0700

    bai 1

---
+++ IT18302/BAI_1_Kieu_Du_Lieu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_1_Kieu_Du_Lieu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            int namSinh = 2004;
+            double canNang = 40.5;
+            float chieuCao = 152;
+            char c = 'H';
+            string hoTen = "HuyenNK6";
+            bool gioiTinh = true; //true- nam- false- nữ
+
+            Console.WriteLine(hoTen);
+            Console.WriteLine("Tên tôi là: "+ hoTen);
+            Console.WriteLine($"Tên tôi là: {hoTen}");
+            Console.WriteLine($"Năm sinh: {namSinh}| tuoi: {2023- namSinh}");
+            Console.WriteLine($"Chiều cao: {chieuCao}| cân nặng: {canNang}");
+        }
+    }
+}
---
+++ IT18302/IT18302/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT18302
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            //cw tab x2 
+            Console.WriteLine("Hello world!!");
+            //giữ ctrl => zoom
+            Console.WriteLine("poly");
+            Console.WriteLine("HN");
+            Console.WriteLine("HN");
+            Console.WriteLine("HN");
+            Console.WriteLine("HN");
+            Console.WriteLine("HN");
+
+            Console.WriteLine("Khánh Huyền");
+
+            //ctrl + F5: chạy CT
+            //ctrl + D: coppy code
+            //ctrl + K + D: format code
+            //ctrl + K + C: comment code
+            //ctrl + K + U: mở comment code
+        }
+    }
+}
